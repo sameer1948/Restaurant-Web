99d787c5dedd7f028b7edcfb2d12072327e0ad30
Sameer <sameer.sameer185@gmail.com>
2024-09-16 22:39:56 +0530

    Removing All Files

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
-export const routes: Routes = [{path:'home', component: HomeComponent},
-/*{path:'profile', component: ProfileComponent},
-{path:'settings', component: SettingsComponent},
-{path:'services', component: BranchComponent}*/
-];
