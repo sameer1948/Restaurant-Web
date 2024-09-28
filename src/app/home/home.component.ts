12b42aae50ba2354de78c53a44089999312b0468
Sameer <sameer.sameer185@gmail.com>
2024-09-29 01:56:46 +0530

    Updating Home Page with Menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,48 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../services/menu.service';
 import { MenuList } from '../model/MenuList';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { MatTableDataSource } from '@angular/material/table';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  animations: [
+    trigger('addRemoveAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-20px)' }),
+        animate('0.5s', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('0.5s', style({ opacity: 0, transform: 'translateY(20px)' }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
-  tiles: Tile[] = [
-    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
-    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
-  ];
-
   menuItems : Array<MenuList> = [];
-
   orderedMenu : Array<MenuList> = [];
 
-  path: string = 'H:\\spring\\Restaurant\\src\\main\\resources\\static\\idly.png';
-
-  displayedColumns: string[] = ['demo-position', 'demo-name', 'demo-weight', 'demo-symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['serial', 'name', 'quantity', 'price','action'];
+  dataSource : any; 
+  finalPrice : number = 0.0;
 
   constructor(private menuService: MenuService) { }
 
@@ -58,16 +43,47 @@ export class HomeComponent implements OnInit {
       )
   }
 
+  searchItem(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    
+  }
+  
   addItem(menu: MenuList) {
     this.orderedMenu.push(menu);
     console.log(this.orderedMenu)
-  }
+    this.finalPrice = this.calculateFinalPrice(this.orderedMenu);
+    this.dataSource = new MatTableDataSource(this.orderedMenu);
     
-}
+  }
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  removeItem(menu: any) {    
+    this.orderedMenu = this.orderedMenu.filter(item => item !== menu);
+    this.dataSource = new MatTableDataSource(this.orderedMenu);
+    this.finalPrice = this.calculateFinalPrice(this.orderedMenu);
+    console.log("remove : " + this.orderedMenu)
+    
+  }
+
+  calculateFinalPrice(items: Array<MenuList>) {
+    let result : number = 0.0;
+    for(let i=0; i<items.length; i++){      
+      result += items[i].price;
+    }
+    return result;
+  }
+
+  checkout(total: number) {
+    console.log(total)    ;
+  }
+
+  clearAll() {
+    
+    this.orderedMenu = [];
+  }
+     
 }
