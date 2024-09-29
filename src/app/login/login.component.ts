e3ed84ac067b3a8fd1141f903cefda54f2a18921
Sameer <sameer.sameer185@gmail.com>
2024-09-30 01:23:05 +0530

    Adding Login Component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  showPassword: boolean = false; // Manage password visibility
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+}
