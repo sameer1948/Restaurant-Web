923034e3ebf38bda7be1215f260b26d961e8cf96
Sameer <sameer.sameer185@gmail.com>
2024-10-24 01:21:41 +0530

    Adding Side - nav Component

---
+++ src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrl: './side-nav.component.scss'
+})
+export class SideNavComponent {
+
+  selectedvalue=25;
+  startvalue=30;
+  endvalue=70;
+}
