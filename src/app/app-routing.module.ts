be8ab7673299b6b5faa6b2df53a14e23b9c23730
Sameer <sameer.sameer185@gmail.com>
2024-10-24 01:30:02 +0530

    Updating Roting and App Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,16 +10,20 @@ import { authGuard } from './guard/auth.guard';
 import { ErrorComponent } from './errors/error/error.component';
 import { roleAdminGuard } from './guard/role-admin.guard';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MenuHomeComponent } from './menu/menu-home/menu-home.component';
 
 const routes: Routes = [
-  {path:'home', component: HomeComponent},
-  //{path:'profile', component: ProfileComponent},
-  {path:'settings', component: SettingsComponent, canActivate : [authGuard]},
-  {path:'services', component: BranchComponent},
-  {path:'ad-home', component: AdminHomeComponent, 
+  { path:'', component: UnauthorizedComponent },
+  { path:'home', component: HomeComponent },  
+  { path : 'menu', component:MenuHomeComponent},
+  { path:'profile', component: ProfileComponent,   canActivate : [authGuard] },
+  { path:'settings', component: SettingsComponent, canActivate : [authGuard] },
+  { path:'services', component: BranchComponent },
+  { path:'ad-home', component: AdminHomeComponent, 
                    canActivate : [authGuard, roleAdminGuard], 
                    data: { expectedRole: 'admin' } },
-  {path:'login', component: LoginComponent},
+  { path:'login', component: LoginComponent },
   { path: 'error', component: ErrorComponent },
   { path: 'unauthorized', component: UnauthorizedComponent },
   { path: '**', component: ErrorComponent }
