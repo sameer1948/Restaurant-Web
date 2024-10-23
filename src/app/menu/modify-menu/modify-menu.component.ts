8cb5c1a2e9421b0e564b43a17ab4ec99d3c92d12
Sameer <sameer.sameer185@gmail.com>
2024-10-24 01:23:34 +0530

    Updating Modify - Component

--- src/app/menu/modify-menu/modify-menu.component.ts
+++ src/app/menu/modify-menu/modify-menu.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, Inject, signal } from '@angular/core';
 import { NotificationService } from '../../common/notification.service';
-import { AdminService } from '../../services/admin.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MenuList } from '../../model/MenuList';
+import { MenuService } from '../../services/menu.service';
 
 @Component({
   selector: 'app-modify-menu',
   templateUrl: './modify-menu.component.html',
   styleUrl: './modify-menu.component.scss'
 })
-export class ModifyMenuComponent implements OnInit {
+export class ModifyMenuComponent {
 
   protected readonly value = signal('');
-  selected:any;
+  
+  selected: MenuList = new MenuList();
 
   form : FormGroup = new FormGroup({ 
     menuItem: new FormGroup({ 
@@ -30,21 +31,13 @@ export class ModifyMenuComponent implements OnInit {
   selectedQty: any;
 
   constructor(private _notificationService : NotificationService,
-    private _adminService : AdminService, 
-    private _matDialogRef :  MatDialogRef<ModifyMenuComponent>,) {}
-
-  ngOnInit(): void {
+    private menuService : MenuService, 
+    private _matDialogRef :  MatDialogRef<ModifyMenuComponent>,
+    @Inject(MAT_DIALOG_DATA) data: MenuList) {
+      this.selected = data;
+      this.onSelect()
+    }
     
-    this._adminService.getAllItems().subscribe(
-      (result) => {
-        this.menuItems = result;
-      }, (error) => {
-        this._notificationService.errorMessage("Error While Fetching Records...!");
-      }
-    );
-
-  }
-
   protected onInput(event: Event) {
     this.value.set((event.target as HTMLInputElement).value);
   }
@@ -58,7 +51,6 @@ export class ModifyMenuComponent implements OnInit {
         qty : this.selected.qty,
         price : this.selected.price,
         itemPngPath : this.selected.itemPngPath,
-
       }
     });
     this.selectedQty = this.selected.qty;
@@ -74,17 +66,16 @@ export class ModifyMenuComponent implements OnInit {
     menuList.price = this.form.value.menuItem?.price;
     menuList.itemPngPath = this.form.value.menuItem?.itemPngPath;
 
-    this._adminService.modifyItemInMenu(menuList).subscribe(
+    this.menuService.modifyItemInMenu(menuList).subscribe(
       (data) => {
-        console.log(data);
-        this.onClose();
+        console.log(data);        
         this._notificationService.successMessage("Sucess");
+        this._matDialogRef.close('sucess');
 
       }, (error) => {
         this._notificationService.errorMessage("Something went wrong while Updating Menu...!")
       }
     );
-  
   }
 
   onClose() {
