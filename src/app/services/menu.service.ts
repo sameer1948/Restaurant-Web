a729b58059b7b4b2206a48289b27788217bc96d9
Sameer <sameer.sameer185@gmail.com>
2024-10-24 01:27:01 +0530

    Updating menu Services

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { MenuList } from '../model/MenuList';
+import { error } from 'console';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,31 @@ export class MenuService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public getAllItems() : Observable<MenuList[]> {
-    const headers = { 'content-type': 'application/json'}  
-    return this.httpClient.post<MenuList[]>(this.MENU_API_URL + 'fetch-all-menu', null, {headers : headers});
+  public getAllItems(): Observable<MenuList[]> {
+    return this.httpClient.post<MenuList[]>(this.MENU_API_URL + 'fetch-all-menu', null, {})
+    .pipe(catchError(this.handleError)); // Handle errors
   }
 
-  public createNew(menuList : MenuList) : Observable<MenuList> {
+  public addItemToMenu(menu : MenuList) : Observable<MenuList> {
     const headers = { 'content-type': 'application/json'}  
-    return this.httpClient.post<MenuList>(this.MENU_API_URL + 'add-menu', menuList, {headers : headers});
+    return this.httpClient.post<MenuList>(this.MENU_API_URL + 'add-menu', menu, {headers : headers});
   }
-  
+
+  public modifyItemInMenu(menu : MenuList) : Observable<MenuList> {
+    const headers = { 'content-type': 'application/json'}  
+    return this.httpClient.patch<MenuList>(this.MENU_API_URL + 'update-menu', menu, {headers : headers});
+  }
+
+  public removeItemInMenu(id: number) : Observable<string> {    
+    return this.httpClient.delete<string>(this.MENU_API_URL + 'delete-menu/' + id);
+  }
+
+
+  private handleError(error: HttpErrorResponse) {
+    // Handle different types of errors (network, server, etc.)
+    console.error('An error occurred:', error.message);
+    return throwError('Something went wrong; please try again later.');
+  }
+
+
 }
