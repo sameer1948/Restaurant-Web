7b3012b8f2449149d91efc9cda0773b7c3bbc115
Sameer <sameer.sameer185@gmail.com>
2024-10-24 13:39:39 +0530

    Adding Members components

---
+++ src/app/member/remove-member/remove-member.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-member',
+  templateUrl: './remove-member.component.html',
+  styleUrl: './remove-member.component.scss'
+})
+export class RemoveMemberComponent {
+
+}
