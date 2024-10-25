174760cc273a8d447e4f88866e0b07b41990258d
Sameer <sameer.sameer185@gmail.com>
2024-10-25 14:42:43 +0530

    Updating modify member Component

--- src/app/member/update-member/update-member.component.ts
+++ src/app/member/update-member/update-member.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
+import { CustomUserDetails } from '../../model/CustomUserDetails';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-update-member',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './update-member.component.scss'
 })
 export class UpdateMemberComponent {
+  user!: CustomUserDetails;
 
+  constructor(private matDialogRef :  MatDialogRef<UpdateMemberComponent>,
+    @Inject(MAT_DIALOG_DATA) data: CustomUserDetails){
+    this.user = data;
+  }
+
+  getRoles(): string[] {
+    return this.user.customUser.roles.split(','); 
+  }
+
+  close() {
+    this.matDialogRef.close();
+  }
 }
