444366e1802fc7c1a2a72d23922b64d96adb1646
Sameer <sameer.sameer185@gmail.com>
2024-11-01 00:50:52 +0530

    Update App Componet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  title = 'restaurant-web';
-  
-  isAuthenticated : boolean = false; 
+  title = 'Restaurant - web';
 
-  constructor() {}
-
-  ngOnInit(): void {
-    
-    /* if (sessionStorage.getItem("token") != null) {
-      this.isAuthenticated = true;
-    } */
+  constructor(private titleService: Title, private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      const pageTitle = this.getPageTitle(this.router.url);
+      this.setTitle(pageTitle);
+    });
   }
- 
-  protected logout() {
-    
-    sessionStorage.removeItem("token");
-    sessionStorage.removeItem("role");
-    this.isAuthenticated  = false; 
+
+  setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
+
+  getPageTitle(url: string): string {
+    switch (url) {
+
+      case '/home':
+        return 'Home';
+
+      case '/members':
+        return 'Members';
+
+      case '/profile':
+        return 'Profile';
+
+      case '/login':
+        return 'Login';
+
+      case '/error':
+        return 'Error';
+
+      case '/coupon':
+        return 'Coupon';
+
+      case '/unauthorized':
+        return 'Unauthorized';
+
+      case '/about':
+        return 'About Us';
+
+      default:
+        return this.title;
+    }
   }
 
 }
