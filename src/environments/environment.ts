8b4c8e954d861bd16c1fe0e7980b56e47c65842d
Sameer <sameer.sameer185@gmail.com>
2024-11-21 00:13:07 +0530

    Update UPI KEY

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://restaurant-api-22y2.onrender.com',
+    apiUrl: 'https://restaurant-api-22y2.onrender.com/',
     featureFlag: false,
 };
